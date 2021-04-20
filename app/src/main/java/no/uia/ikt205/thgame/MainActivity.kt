fe59aaa2c16c788e360a67398cc156008aa22234
Christian Simonsen <crsdev@gmail.com>
2021-04-21 01:16:34 +0200

    example

---
+++ app/src/main/java/no/uia/ikt205/thgame/MainActivity.kt
@@ -0,0 +1,51 @@
+package no.uia.ikt205.knotsandcrosses
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import no.uia.ikt205.knotsandcrosses.api.GameService
+import no.uia.ikt205.knotsandcrosses.databinding.ActivityMainBinding
+import no.uia.ikt205.thgame.dialogs.CreateGameDialog
+import no.uia.ikt205.thgame.dialogs.GameDialogListener
+
+class MainActivity : AppCompatActivity() , GameDialogListener {
+
+    val TAG:String = "MainActivity"
+
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startGameButton.setOnClickListener {
+            createNewGame()
+        }
+
+        binding.joinGameButton.setOnClickListener {
+            joinGame()
+        }
+
+    }
+
+    private fun createNewGame(){
+        val dlg = CreateGameDialog()
+        dlg.show(supportFragmentManager,"CreateGameDialogFragment")
+    }
+
+    private fun joinGame(){
+
+    }
+
+    override fun onDialogCreateGame(player: String) {
+        Log.d(TAG,player)
+    }
+
+    override fun onDialogJoinGame(player: String, gameId: String) {
+        Log.d(TAG, "$player $gameId")
+    }
+
+}
---
+++ app/src/main/java/no/uia/ikt205/thgame/dialogs/CreateGameDialog.kt
@@ -0,0 +1,58 @@
+package no.uia.ikt205.thgame.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import no.uia.ikt205.knotsandcrosses.R
+import no.uia.ikt205.knotsandcrosses.databinding.ActivityGameBinding
+import no.uia.ikt205.knotsandcrosses.databinding.DialogCreateGameBinding
+import java.lang.ClassCastException
+
+class CreateGameDialog() : DialogFragment() {
+
+    internal lateinit var listener:GameDialogListener
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+
+            val builder: AlertDialog.Builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+            val binding = DialogCreateGameBinding.inflate(inflater)
+
+            builder.apply {
+                setTitle("Create game")
+                setPositiveButton("Create") { dialog, which ->
+                    if(binding.username.text.toString() != ""){
+                        listener.onDialogCreateGame(binding.username.text.toString())
+                    }
+                }
+                setNegativeButton("Cancle") { dialog, which ->
+                    dialog.cancel()
+                }
+                setView(binding.root)
+            }
+
+            builder.create()
+
+
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as GameDialogListener
+        } catch (e:ClassCastException){
+            throw ClassCastException(("$context must implement GameDialogListener"))
+
+        }
+    }
+
+}
